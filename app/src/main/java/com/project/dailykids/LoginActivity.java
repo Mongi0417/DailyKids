7570a5c5c0e113c50d4d70aba7ac3ba0937d3ac9
Hub0128 <bsh4128@naver.com>
2022-12-15 16:06:34 +0900

    Activity for Home

---
+++ app/src/main/java/com/project/dailykids/LoginActivity.java
@@ -0,0 +1,106 @@
+package com.project.dailykids;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+public class LoginActivity extends AppCompatActivity {
+    private View mView;
+    private TextView tvToolbarTitle;
+    private Toolbar toolbar;
+    private EditText edtEmail, edtPassword;
+    private Button btnLogin, btnRegister, btnTrial;
+    /*private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;*/
+    private long waitTime = 0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.login_layout);
+
+        mView = findViewById(R.id.toolbar_login);
+        toolbar = mView.findViewById(R.id.toolbar);
+        tvToolbarTitle = mView.findViewById(R.id.tvToolbarTitle);
+        edtEmail = findViewById(R.id.login_edtEmail);
+        edtPassword = findViewById(R.id.login_edtPassword);
+        btnLogin = findViewById(R.id.btnLogin);
+        btnRegister = findViewById(R.id.btnRegister);
+        btnTrial = findViewById(R.id.btnTrial);
+        /*mAuth = FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(FirebaseAuth mAuth) {
+                FirebaseUser user = mAuth.getCurrentUser();
+                if (user != null) {
+                    Intent intent = new Intent(LoginActivity.this, HomeActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+            }
+        };*/
+
+        tvToolbarTitle.setText("로그인");
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!TextUtils.isEmpty(edtEmail.getText().toString()) && !TextUtils.isEmpty(edtPassword.getText().toString())) { // NullPointerException 발생 위험성 있으므로 TextUtils 활용
+                    //loginUser(edtEmail.getText().toString(), edtPassword.getText().toString());
+                } else {
+                    Toast.makeText(LoginActivity.this, "아이디와 비밀번호를 확인해주세요.", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        /*btnRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }
+        });*/
+    }
+
+    /*private void loginUser(String id, String password) {
+        mAuth.signInWithEmailAndPassword(id, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(Task<AuthResult> task) {
+                if (task.isSuccessful()) { // 로그인 성공 시,
+                    mAuth.addAuthStateListener(mAuthListener);
+                } else// 로그인 실패 시,
+                    Toast.makeText(LoginActivity.this, "아이디 또는 비밀번호가 일치하지 않습니다.", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }*/
+
+    @Override
+    public void onBackPressed() {
+        if (System.currentTimeMillis() - waitTime >= 2000) {
+            waitTime = System.currentTimeMillis();
+            Toast toast = Toast.makeText(LoginActivity.this, "뒤로 버튼을 한번 더 누르시면 종료됩니다.", Toast.LENGTH_SHORT);
+            toast.setGravity(Gravity.CENTER, 0, 0);
+            toast.show();
+        } else {
+            finish();
+        }
+    }
+
+    /*@Override
+    protected void onStop() {
+        super.onStop();
+        if (mAuthListener != null)
+            mAuth.removeAuthStateListener(mAuthListener);
+    }*/
+}
---
+++ app/src/main/java/com/project/dailykids/RegisterStep1Activity.java
@@ -0,0 +1,12 @@
+package com.project.dailykids;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RegisterStep1Activity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
